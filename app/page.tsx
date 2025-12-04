6db884374d71ed08e13d23a4cd52f1dcfe63af56
Aayush singh <saayush615@gmail.com>
2025-12-04 23:06:28 +0530

    feat: Add LightRay component and implemted it

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 const page = () => {
   return (
-    <div>
-      <h1>Welcome to Nextjs</h1>
-    </div>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br /> Event You can't Miss</h1>
+      <p className="text-center mt-5">Hackathon, Meetups, and Confrences, All in one Places</p>
+    </section>
   )
 }
 

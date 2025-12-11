ac474e8412c04e66b13d98a2b42cbaa307f7176b
Aayush singh <saayush615@gmail.com>
2025-12-11 21:33:21 +0530

    feat: Add ExploreBtn client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You can't Miss</h1>
       <p className="text-center mt-5">Hackathon, Meetups, and Confrences, All in one Places</p>
+
+      <ExploreBtn />
     </section>
   )
 }

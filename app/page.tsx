b9c90e4fede11656e1c30bce5b5f7268c1ddf73d
Aayush singh <saayush615@gmail.com>
2025-12-11 23:26:51 +0530

    feat: Add basic Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
+import {events} from '@/lib/constants';
 
 const page = () => {
   return (
@@ -7,6 +9,20 @@ const page = () => {
       <p className="text-center mt-5">Hackathon, Meetups, and Confrences, All in one Places</p>
 
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>
+          Feature Events
+        </h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
